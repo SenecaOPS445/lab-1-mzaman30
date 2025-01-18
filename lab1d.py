bc03f675dc26aafb453342858145dc873b339a53
Md Shahriar Zaman <mzaman30@myseneca.ca>
2025-01-18 08:45:03 -0500

    Added lab1d for math operations

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program demonstrates basic math operations in Python.'''
+
+# Declare variables
+x = 10
+y = 2
+z = 5
+
+# Perform and print the result of the expression
+print(f"{x} + {y} * {z} = {x + y * z}")
