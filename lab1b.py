8c260d127dcdc1d9f4a3b483be6d9c6da4fd1b68
Md Shahriar Zaman <mzaman30@myseneca.ca>
2025-01-18 08:27:44 -0500

    Added lab1b.py and lab1c.py

---
+++ lab1b.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+'''Description: This program will ask for a friend's name and display a message.'''
+
+name = 'Isaac'
+print('How old are you ' + name + '?')
---
+++ lab1c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Print sum with a string (convert integer to string)
+name = 'Isaac'
+age = 72
+
+# Print the result with name and age
+print(name + ' is ' + str(age) + ' years old!')
